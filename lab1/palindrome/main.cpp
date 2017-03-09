eaac9346234da8372fd198ad0420e94e96453eaf
mmamica <maria.anna.mamica@gmail.com>
2017-03-09 12:04:27 +0100

    commit

--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -2,13 +2,28 @@
 #include<string>
 #include "Palindrome.h"
 using namespace std;
-int main(){
-    string str;
-    cin >>str;
-    if (is_palindrome(str)){
-        cout<<"tak"<<endl;
+int main() {
+    bool flag= true;
+    while (flag) {
+        cout << "If you want to check the word press 'c' or 'q' if you want to leave" << endl;
+        char user_choice;
+        cin >> user_choice;
+        if (user_choice == 'c') {
+            cout << "Give the word" << endl;
+            string str;
+            cin >> str;
+            if (is_palindrome(str)) {
+                cout << "yes" << endl;
+            } else {
+                cout << "no" << endl;
+            }
+        }
+        if (user_choice == 'q') {
+            flag=false;
+        } else {
+            flag=true;
+        }
+
     }
-    else{
-        cout<<"nie"<<endl;
-    }
+    return 0;
 }
