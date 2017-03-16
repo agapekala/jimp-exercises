89474a7ad923c79d4a66132d741004a8675c477d
mmamica <maria.anna.mamica@gmail.com>
2017-03-16 10:06:21 +0100

    commit

--- lab2/array2d/Array2D.cpp
+++ lab2/array2d/Array2D.cpp
@@ -1,58 +1,55 @@
 //
-// Created by Maria on 11.03.2017.
+// Created by DELL on 11.03.2017.
 //
 
-#include<iostream>
 #include "Array2D.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-using namespace std;
 
-int **NewArray2D(int n_rows, int n_cols) {
-    if (n_rows <= 0 || n_cols <= 0) {
-        int **array = nullptr;
-        return array;
-    } else {
-        int **array=new int* [n_rows];
-        for (int i = 0; i < n_rows; i++) {
-            int *array = new int[n_cols];
-        }
+void FillArray2D(int n_rows, int n_columns, int *const *array);
+
+int **NewArray2D(int n_rows, int n_columns);
+
+int **Array2D(int n_rows, int n_columns){
+    return NewArray2D(n_rows, n_columns);
+}
+
+int **NewArray2D(int n_rows, int n_columns) {
+    if(n_rows <= 0 || n_columns <= 0){
+        int **array=nullptr;
         return array;
     }
+    int **array=new int*[n_rows];
+    for(int i=0; i<n_rows; i++){
+        array[i]=new int[n_columns];
+    }
+    FillArray2D(n_rows, n_columns, array);
+    return array;
 }
 
-int **Array2D(int n_rows, int n_cols){
-    return NewArray2D(n_rows, n_cols);
-}
-
-void FillArray2D(int n_rows, int n_cols, int *const *array){
-    int a=1;
-    for (int i=0; i++; i<n_rows) {
-        for (int j = 0; j++; j < n_cols) {
-            array[i][j] = a;
-            cin >> a;
-            a++;
+void FillArray2D(int n_rows, int n_columns, int *const *array) {
+    int c=1;
+    for(int i=0; i<n_rows; i++){
+        for(int j=0; j<n_columns; j++){
+            array[i][j]=c;
+            c++;
         }
     }
 }
 
-void PtintArray2D(int **array, int n_rows, int n_cols){
-    for(int i=0; i<n_rows; i++){
-        for(int j=0; j<n_cols; j++){
-            cout<<array[i][j];
-            cout<<" ";
-            }
-        cout<<endl;
-    }
-}
-
-void DeleteArray2D(int **array, int n_rows, int n_cols){
-    for(int i=0; i<n_rows; i++){
+void DeleteArray2D(int **array, int n_rows, int n_columns){
+    int i;
+    for(i=0; i<n_rows; i++){
         delete [] array[i];
     }
     delete [] array;
 }
 
-
-
+void PrintArray2D(int n_rows,int n_columns,int **array){
+    int i,j;
+    for(i=0; i<n_rows; i++){
+        for(j=0; j<n_columns; j++){
+            cout<<array[i][j];
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
--- lab2/array2d/Array2D.h
+++ lab2/array2d/Array2D.h
@@ -1,21 +1,16 @@
 //
-// Created by Maria on 11.03.2017.
+// Created by DELL on 11.03.2017.
 //
 
 #ifndef JIMP_EXERCISES_ARRAY2D_H
 #define JIMP_EXERCISES_ARRAY2D_H
 
-
-#include<iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include "Array2D.h"
+#include <iostream>
 using namespace std;
-int **NewArray2D(int n_rows, int n_cols);
-int **Array2D(int n_rows, int n_cols);
-void FillArray2D(int n_rows, int n_cols, int *const *array);
-void PtintArray2D(int **array, int n_rows, int n_cols);
-void DeleteArray2D(int **array, int n_rows, int n_cols);
+int **Array2D(int n_rows, int n_columns);
+void DeleteArray2D(int **array, int n_rows, int n_columns);
+void FillArray2D(int n_rows, int n_columns, int *const *array);
+int **NewArray2D(int n_rows, int n_columns);
+void PrintArray2D(int n_rows,int n_columns,int **array);
 
 #endif //JIMP_EXERCISES_ARRAY2D_H
