a6cbe403f6d2cb34c47879046724b6c58969bc2e
mmamica <maria.anna.mamica@gmail.com>
2017-03-16 10:08:31 +0100

    commit

--- lab2/polybius/Polybius.cpp
+++ lab2/polybius/Polybius.cpp
@@ -1,75 +1,74 @@
 //
-// Created by Maria on 13.03.2017.
+// Created by DELL on 11.03.2017.
 //
 
 #include "Polybius.h"
-#include<iostream>
 #include <string>
-#include <map>
-#include <w32api/wsman.h>
 
-std::string PolybiusCrypt(std::string message);
-std::string PolybiusDecrypt(std::string crypted);
-using namespace std;
 
-map<char, string> MakeAlphabet(){
-    char tab[4][4];
-    int number=65;
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            tab[i][j]=(char)number;
+std::string PolybiusCrypt(std::string message){
+    std::string str="";
+    int n_first, n_second;
+    std::string l_first, l_second;
+    for(int i=0; i<message.length(); i++){
+        if(message[i]==' '){
+            i++;
         }
-    }
-    map<char, string> alfabet;
-    int c=65;
-    for (int i=0; i<5; i++){
-        for (int j =0; j<5; j++){
-            string kluczyk = to_string(i)+to_string(j);
-            char z=(char)c;
-            if(c==73){
-                alfabet[z]=kluczyk;
-                c++;
-                char z=(char)c;
-                alfabet[z]=kluczyk;
-                c++;
+        if(message[i]>=97 && (message[i])<=122){
+           message[i]=(message[i]-32);
+        }
+        if(message[i]>73){
+            if((message[i]-65)%5==0){
+                n_first=(message[i]-65)/5;
             }
             else{
-                alfabet[z]=kluczyk;
-                c++;
+                n_first =(((message[i]) - 65)/5)+1;
             }
-
-        }
-    }
-    return alfabet;
-}
-
-string PolybiusCrypt(string message){
-    int i=0;
-    string hidden_message="";
-    while(!file.eof()){
-        if(int(message[i])>=65 || int(message[i])<=90){
-            hidden_message=hidden_message+MakeAlphabet()[message[i]];
+            if((message[i]-65)%5==0){
+                n_second=5;
+            }
+            else{
+                n_second = (((message[i]) - 65)%5);
+            }
         }
         else{
-            hidden_message=hidden_message+message[i];
+            if((message[i]-64)%5==0){
+                n_first=(message[i]-64)/5;
+            }
+            else{
+                n_first =(((message[i]) - 64)/5)+1;
+            }
+            if((message[i]-64)%5==0){
+                n_second=5;
+            }
+            else{
+                n_second = (((message[i]) - 64)%5);
+            }
         }
-        i++;
+        l_first = std::to_string( n_first );
+        l_second= std::to_string( n_second );
+        str +=l_first;
+        str +=l_second;
     }
-    return hidden_message;
+    return str;
 }
 
-string PolybiusDecrypt(string crypted){
-    int i=0;
-    string message="";
-    while(!file.eof()){
-        map<char, string> alfabet=MakeAlphabet();
-        if(int(crypted[i])>=65 || int(crypted[i])<=90){
-            message=message+alfabet.find(crypted[i]);
+
+std::string PolybiusDecrypt(std::string crypted){
+    std::string str="";
+    int n_first, n_second;
+    char letter;
+    for(int i=0; i<crypted.length(); i+=2){
+        n_first=crypted[i]-48;
+        n_second=crypted[i+1]-48;
+        letter=n_second+(n_first-1)*5;
+        if(letter>9){
+            letter+=97;
         }
-        else{
-            message=message+crypted[i];
+        else {
+            letter += 96;
         }
-        i++;
+        str+=letter;
     }
-    return message;
+    return str;
 }
